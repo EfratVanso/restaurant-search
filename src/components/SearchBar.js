8b31acb1c948a6913e1eb524927ecef574a0b8c1
efiVanso <efi.vanso@gmail.com>
2020-11-30 23:16:03 +0200

    add icons
    from https://icons.expo.fyi/

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-
+import {Feather} from '@expo/vector-icons';
 const SearchBar = () => {
   return (
     <View style={styles.background}>
+        <Feather name="search" size={30} />
       <Text>Search Bar</Text>
     </View>
   );
-};
+}; 
 
 const styles = StyleSheet.create({
     background:{
