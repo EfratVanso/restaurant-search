bd819127c73946c3784b5b6cb892b87648e6b17f
efiVanso <efi.vanso@gmail.com>
2020-11-30 22:14:14 +0200

    add react navigation

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import {createStackNavigator, createAppContainer} from 'react-navigation';
+// import searchScreen from './src/screens/searchScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// const navigator = createStackNavigator({
+// Search :searchScreen,
+// },
+// {
+//   initialRoutName:'Search', // default screen
+//   defaultNavigationOptions:{
+//     title:'Business Search' //default title on top of the screen in the app
+//   }
+// });
+// export default createAppContainer(navigator);//app.js must export react component
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import SearchScreen from './src/screens/SearchScreen';
+
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen,
   },
-});
+  {
+    initialRouteName: 'Search',
+    defaultNavigationOptions: {
+      title: 'Business Search',
+    },
+  }
+);
+
+export default createAppContainer(navigator);
