09e1a5bfc29d1083ea3884ff10fbaef281cebba2
efiVanso <efi.vanso@gmail.com>
2020-12-03 22:27:21 +0200

    Communicating Between Screens &
    Fetching a Single Restaurant

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import yelp from '../api/yelp';
 
-export default function ResultsShowScreen() {
+export default function ResultsShowScreen({navigation}) {
+    const [results, setResults] = useState(null);
+    const id = navigation.getParam('id');
+    console.log(results)
+    const getResult = async(id)=>{
+        const response = await yelp.get(`/${id}`);
+        setResults(response.data)
+    }
+    useEffect(() => {// run when first rendered
+        getResult(id)
+    }, []);
+
     return (
         <View>
-            <Text>jughjg</Text>
+            <Text>{JSON.stringify(results) }</Text>
         </View>
     )
 }
