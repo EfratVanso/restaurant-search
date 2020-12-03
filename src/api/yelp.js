41cba24b28b91dc54b9b19b91a295f42e2b6a7c3
efiVanso <efi.vanso@gmail.com>
2020-12-03 23:04:42 +0200

    api documentation

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -7,3 +7,6 @@ export default axios.create({ //pre set of options
       'Bearer 552jLZkdTXf1nFMQJKhd4Ev5N2ulounD310g-GrH0JSOG0HLTa4iXRmOfKNqLfz2vZOhHHWukMj60ZN4pmhyHMqTdXXP5n1waNgPp93A7rqc794DSvhpQTf3AMLCX3Yx'
   }
 });
+
+// api documentation:
+// https://www.yelp.com/developers/documentation/v3
