d40f925106173507bbade6217972652f1454ef4f
efiVanso <efi.vanso@gmail.com>
2020-12-03 20:02:41 +0200

    fix scrolling
    adding flex:1 fixes the problem of overload content

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
@@ -16,7 +16,7 @@ const SearchScreen = () => {
     });
   };
   return (
-    <View>
+    <View style={{flex:1}}> 
       <SearchBar
         term={term}
         onTermChange={(newTerm) => setTerm(newTerm)}
@@ -24,10 +24,12 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>we have found {results.length} results</Text>
-
-      <ResultsList results={filterResultsByPrice('$')} title="Cost Effective " />
-      <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
-      <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
+ {/* enable to scroll on small screens  */}
+      <ScrollView> 
+        <ResultsList results={filterResultsByPrice('$')} title="Cost Effective " />
+        <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+        <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
+      </ScrollView>
     </View>
   );
 };
