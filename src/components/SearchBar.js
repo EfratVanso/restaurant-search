d5cd864af4630e04820853a5aede8a21cf30edff
efiVanso <efi.vanso@gmail.com>
2020-12-03 11:12:38 +0200

    Error Handling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,43 +1,41 @@
-import React from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
-import {Feather} from '@expo/vector-icons';
-const SearchBar = ({term, onTermChange, onTermSubmit}) => {
+import React from "react";
+import { View, TextInput, StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons";
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
   return (
     <View style={styles.backgroundStyle}>
-        <Feather name="search" style={styles.iconStyle} />
-      <TextInput 
-      style={styles.InputStyle} 
-      placeholder="search"
-      value={term}
-      onChangeText={ newTerm => onTermChange(newTerm)}
-      autoCapitalize="none"
-      autoCorrect={false}
-      onEndEditing={()=>onTermSubmit()}
+      <Feather name="search" style={styles.iconStyle} />
+      <TextInput
+        style={styles.InputStyle}
+        placeholder="search"
+        value={term}
+        onChangeText={(newTerm) => onTermChange(newTerm)}
+        autoCapitalize="none"
+        autoCorrect={false}
+        onEndEditing={() => onTermSubmit()}
       />
     </View>
   );
-}; 
+};
 
 const styles = StyleSheet.create({
-    backgroundStyle:{
-        marginTop: 10,
-        backgroundColor:'#F0EEEE',
-        height:50,
-        borderRadius:5,
-        marginHorizontal:15,
-        flexDirection: 'row-reverse',
-        
-    },
-    InputStyle:{
-        flex: 1, //take most of the space in the view 
-        fontSize: 18
-    },
-    iconStyle:{
-        fontSize:35,
-        alignSelf:'center',
-        marginHorizontal:15
-
-    }
+  backgroundStyle: {
+    marginTop: 10,
+    backgroundColor: "#F0EEEE",
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: "row-reverse",
+  },
+  InputStyle: {
+    flex: 1, //take most of the space in the view
+    fontSize: 18,
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
+    marginHorizontal: 15,
+  },
 });
 
 export default SearchBar;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,9 +6,11 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const searchApi = async () => {
-    const response = await yelp.get('/search', {
+    try {
+      const response = await yelp.get('/search', {
       params: {
         limit: 50,
         term, //instead term:term
@@ -16,6 +18,9 @@ const SearchScreen = () => {
       }
     });
     setResults(response.data.businesses);
+  } catch (err) {
+    setErrorMessage('Something went wrong');
+  }
   }
 
   return (
@@ -24,7 +29,7 @@ const SearchScreen = () => {
         onTermChange = {newTerm => setTerm(newTerm)}
         onTermSubmit = {()=>  searchApi()}
       />
-      <Text>Search Screen</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>we have found {results.length} results</Text>
     </View>
   );
