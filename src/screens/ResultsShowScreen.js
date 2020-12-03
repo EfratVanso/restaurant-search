c8a55338462e5dd99512cfaada6b21208a658d7a
efiVanso <efi.vanso@gmail.com>
2020-12-03 22:41:27 +0200

    Showing a List of Images

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,27 +1,37 @@
-import React, { useState, useEffect } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import yelp from '../api/yelp';
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, Image, FlatList } from "react-native";
+import yelp from "../api/yelp";
 
-export default function ResultsShowScreen({navigation}) {
-    const [results, setResults] = useState(null);
-    const id = navigation.getParam('id');
-    console.log(results)
-    const getResult = async(id)=>{
-        const response = await yelp.get(`/${id}`);
-        setResults(response.data)
-    }
-    useEffect(() => {// run when first rendered
-        getResult(id)
-    }, []);
+export default function ResultsShowScreen({ navigation }) {
+  const [result, setResult] = useState(null);
+  const id = navigation.getParam("id");
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+  useEffect(() => {    // run when first rendered
+    getResult(id);
+  }, []);
 
-    return (
-        <View>
-            <Text>{JSON.stringify(results) }</Text>
-        </View>
-    )
+  if(!result){
+    return null;
+}
+  return (
+    <View>
+      <Text>{result.name}</Text>
+      <FlatList
+          data = {result.photos}
+          keyExtractor={photo => photo}
+          renderItem={({item}) =>{
+              return <Image style={styles.image} source= {{uri: item}} />
+          }}
+          />
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-    
-})
-
-
+    image:{
+        height:200,
+        width:200
+    }
+});
